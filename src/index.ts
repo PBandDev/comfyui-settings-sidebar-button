e2835c73feddc396029408e0f6d9391bc82f307c
pbanddev <202373785+PBandDev@users.noreply.github.com>
2025-11-23 15:18:09 -0600

    Enhance Settings Sidebar Button with configurable options and improved placement logic

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,64 @@ declare global {
 const BUTTON_ID = "settingsSidebarButton";
 
 app.registerExtension({
-  name: "settingsSidebarButton",
+  name: "Settings Sidebar Button",
+  settings: [
+    {
+      id: `${BUTTON_ID}.Enabled`,
+      name: "Enabled",
+      type: "boolean",
+      defaultValue: true,
+    },
+    {
+      id: `${BUTTON_ID}.PinToBottom`,
+      name: "Pin to Bottom",
+      type: "boolean",
+      tooltip: "Pin the button to the bottom of the sidebar",
+      defaultValue: true,
+    },
+    {
+      id: `${BUTTON_ID}.ShowText`,
+      name: "Show Text",
+      type: "boolean",
+      defaultValue: true,
+      tooltip: "Show the text 'Settings' on the button",
+    },
+  ],
   async setup() {
+    if (!app.extensionManager.setting.get(`${BUTTON_ID}.Enabled`)) {
+      return;
+    }
+
     app.extensionManager.registerSidebarTab({
       id: BUTTON_ID,
       icon: "pi pi-cog",
-      title: "Settings",
+      title: "",
       tooltip: "Open Settings",
       type: "custom",
       render: (_el) => {},
     });
 
     const setupButton = (button: HTMLElement): void => {
+      if (app.extensionManager.setting.get(`${BUTTON_ID}.PinToBottom`)) {
+        const endBar =
+          document.querySelector<HTMLElement>(".side-tool-bar-end");
+        if (endBar) {
+          endBar.appendChild(button);
+        } else {
+          const observer = new MutationObserver(() => {
+            const foundEndBar =
+              document.querySelector<HTMLElement>(".side-tool-bar-end");
+
+            if (foundEndBar) {
+              observer.disconnect();
+              foundEndBar.appendChild(button);
+            }
+          });
+
+          observer.observe(document.body, { childList: true, subtree: true });
+        }
+      }
+
       const observer = new MutationObserver((mutations) => {
         for (const mutation of mutations) {
           if (
